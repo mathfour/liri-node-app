69fae7cd7562e87c584bc9c9d443389bde9405e1
Bon Crowder <SilverDash@Bons-MacBook-Pro.local>
2017-05-10 01:45:08 -0500

    tweets working; cases are set up and ready for functions for the other three

---
+++ liri.js
@@ -0,0 +1,87 @@
+// bmc: todo Grab the data from keys.js
+// bmc: todo Store keys in variables
+
+// Make it so liri.js can take in one of the following commands:
+// my-tweets
+//
+// spotify-this-song
+//
+// movie-this
+//
+// do-what-it-says
+var Twitter = require('twitter');
+var spotify = require("spotify");
+var request = require("request");
+var inquirer = require('inquirer');
+
+var twitterKeyFile = require("./keys.js");
+
+var client = new Twitter({
+    consumer_key: twitterKeyFile.twitterKeys.consumer_key,
+    consumer_secret: twitterKeyFile.twitterKeys.consumer_secret,
+    access_token_key: twitterKeyFile.twitterKeys.access_token_key,
+    access_token_secret: twitterKeyFile.twitterKeys.access_token_secret
+});
+var params = {screen_name: 'mathfour'};
+
+
+// bmc: Need to change this input to one of the fancy dropdowns
+
+    var whatToDo = [
+    {
+        type: "list",
+        message: "pick one",
+        name: "whatToDo",
+        choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+    }
+    ];
+
+inquirer.prompt(whatToDo).then (function(whatTheyWannaDo) {
+    console.log(whatTheyWannaDo);
+    var decision = whatTheyWannaDo.whatToDo;
+
+    switch (decision) {
+        case "my-tweets" :
+            tweetIt();
+            console.log("tweet it");
+            break;
+        case "spotify-this-song" :
+            singIt();
+            console.log("sing it");
+            break;
+        case "movie-this" :
+            watchIt();
+            console.log("watch it");
+            break;
+        case "do-what-it-says":
+            doIt();
+            console.log("do it");
+            break;
+    }
+});
+
+function tweetIt() {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        var i=0;
+        if (!error) {
+            console.log("The last 10 tweets for @MathFour are: ");
+            do{
+                console.log(i+1 + ". " + tweets[i].text);
+                i++;
+            } while (i<10);
+        }
+    });
+};
+
+function singIt() {
+    console.log("launch singIt function");
+}
+
+function watchIt() {
+    console.log("launch watchIt function");
+}
+
+function doIt() {
+    console.log("launch doIt function");
+}
+
